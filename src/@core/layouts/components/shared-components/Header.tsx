bcd74ebbf56878e7784f7f219728c345119a5f2f
abhinav3256 <abhinavkumar3256@gmail.com>
2023-10-06 13:53:00 +0530

    changes06-10-23

--- src/@core/layouts/components/shared-components/Header.tsx
+++ src/@core/layouts/components/shared-components/Header.tsx
@@ -86,7 +86,7 @@ const Header = () => {
                     </div>
                   </div>
 
-                  <button
+                  {/* <button
                     className='navbar-toggler'
                     type='button'
                     data-bs-toggle='collapse'
@@ -96,7 +96,7 @@ const Header = () => {
                     aria-label='Toggle navigation'
                   >
                     <span className='navbar-toggler-icon'></span>
-                  </button>
+                  </button> */}
 
 
                 </div>
