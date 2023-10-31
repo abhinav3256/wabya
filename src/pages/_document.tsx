c0d9e5ea7d9af8df0e0ae71397407aeaf5202626
abhinav3256 <abhinavkumar3256@gmail.com>
2023-10-31 21:11:34 +0530

    changes31-10-23

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,7 +23,7 @@ class CustomDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+        
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           {/* <link
