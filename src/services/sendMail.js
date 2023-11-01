80b755831e4f11bd0420a4bb9c8ac6cef9f40b10
abhinav3256 <abhinavkumar3256@gmail.com>
2023-11-01 10:16:38 +0530

    changes01-11-23

--- src/services/sendMail.js
+++ src/services/sendMail.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export const sendMail = async (to_mail,sub,msg) => {
   try {
     let request = await axios
-      .post("http://157.245.36.128:4502/api/email", {           
+      .post("https://email-api-z7re.onrender.com/api/email", {           
         email: to_mail,	    		  
         subject: sub,     
         message: msg,   		  
