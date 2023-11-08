368502635a1ce87227c349f59e978a98b0b1ae36
abhinav3256 <abhinavkumar3256@gmail.com>
2023-11-08 15:03:31 +0530

    changes08-11-23

--- src/views/calender/Calender.tsx
+++ src/views/calender/Calender.tsx
@@ -1982,7 +1982,7 @@ m=0;
 
 
           <div className="timesheet-carousel">
-          {/* <OwlCarousel options={options}> */}
+          <OwlCarousel options={options}>
 
           { forloops.map((floop, index) => {
             let i=(index)*7;
@@ -2003,7 +2003,7 @@ m=0;
             }
 
           })}
-          {/* </OwlCarousel> */}
+          </OwlCarousel>
           </div>
 
 
